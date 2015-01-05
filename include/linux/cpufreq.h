a410846562451e4308c8cb68379845bb15644826
HolyAngel <slverwolf@gmail.com>
2015-01-04 21:30:22 -0800

    Cpufreq: Add IntelliMinMax Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -406,6 +406,9 @@ extern struct cpufreq_governor cpufreq_gov_lagfree;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
+extern struct cpufreq_governor cpufreq_gov_intellimm;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
 
 #endif
 
