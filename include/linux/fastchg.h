70eb50f17c02d3617ab17a47bcac617c1b40b02c
HolyAngel <slverwolf@gmail.com>
2015-02-11 13:13:42 -0600

    Update FastCharge to latest port

--- include/linux/fastchg.h
+++ include/linux/fastchg.h
@@ -1,8 +1,11 @@
 /*
- * based on work from:
- *	Chad Froebel <chadfroebel@gmail.com> &
- *	Jean-Pierre Rasquin <yank555.lu@gmail.com>
- * for backwards compatibility
+ * Ported to Note 3 (n9005) and extended:
+ * Jean-Pierre Rasquin <yank555.lu@gmail.com>
+ *
+ * Adapted for 9505 from Note 3:
+ * Paul Reioux <reioux@gmail.com>
+ *
+ * Modded by ktoonsez from Jean-Pierre and Faux's original implementation:
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -15,23 +18,50 @@
  *
  */
 
+
 #ifndef _LINUX_FASTCHG_H
 #define _LINUX_FASTCHG_H
 
+#define FAST_CHARGE_VERSION "v1.1 by Ktoonsez from Jean-Pierre and Paul Reioux original implementation"
+#define AC_LEVELS  "500 1000 1450 1900"
+#define USB_LEVELS "460 700 1000"
+#define ANY_LEVELS "0 to 1900"
+
 extern int force_fast_charge;
-extern int fast_charge_level;
 
-#define FAST_CHARGE_DISABLED		0	/* default */
-#define FAST_CHARGE_FORCE_AC		1
-#define FAST_CHARGE_FORCE_CUSTOM_MA	2
+#define FAST_CHARGE_DISABLED 0	/* default */
+#define FAST_CHARGE_FORCE_AC 1
+#define FAST_CHARGE_FORCE_CUSTOM_MA 2
 
-#define FAST_CHARGE_500		500
-#define FAST_CHARGE_900		900
-#define FAST_CHARGE_1200	1200
-#define FAST_CHARGE_1500	1500
-#define FAST_CHARGE_2000	2000
+extern int use_mtp_during_fast_charge;
 
-#define FAST_CHARGE_LEVELS	"500 900 1200 1500 2000"
+#define USE_MTP_DURING_FAST_CHARGE_DISABLED 0
+#define USE_MTP_DURING_FAST_CHARGE_ENABLED 1	/* default */
+
+extern int screen_on_current_limit;
+
+#define SCREEN_ON_CURRENT_LIMIT_DISABLED 0
+#define SCREEN_ON_CURRENT_LIMIT_ENABLED 1	/* default */
+
+extern int ac_charge_level;
+
+#define AC_CHARGE_500 500
+#define AC_CHARGE_1000 1000
+#define AC_CHARGE_1450 1450
+#define AC_CHARGE_2200 2200
+
+extern int usb_charge_level;
+
+#define USB_CHARGE_460 460	/* default */
+#define USB_CHARGE_700 700
+#define USB_CHARGE_1000 1000
+
+/* Whatever happens, this is the limit */
+#define MAX_CHARGE_LEVEL 2200
+
+extern int failsafe;
+#define FAIL_SAFE_ENABLED 1	/* default */
+#define FAIL_SAFE_DISABLED 0
 
 #endif
 
